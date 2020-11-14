66a481d7a22e351d7a206f45177cc8341c16621e
ItsHypers <59265736+ItsHypers@users.noreply.github.com>
2020-11-14 00:46:20 +0000

    EnemyAI

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform player;
+    private Rigidbody2D rb;
+    public float movespeed = 5f;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.Normalize();
+        movement = direction;
+    }
+    
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * movespeed * Time.deltaTime));
+    }
+}

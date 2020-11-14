fb9986a6d0077b2f1d05023da28818be55e0f210
ItsHypers <59265736+ItsHypers@users.noreply.github.com>
2020-11-14 00:17:37 +0000

    Movement Script

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public float movespeed;
+    public float sprintspeed;
+    private float speed;
+    public Rigidbody2D rb;
+    private Vector2 md;
+
+    void Awake()
+    {
+        speed = movespeed;
+    }
+    
+    void Update()
+    {
+        ProcessInputs();
+
+        if(Input.GetButtonDown("Shift"))
+        {
+            speed = sprintspeed;
+        }
+        if(Input.GetButtonUp("Shift"))
+        {
+            speed = movespeed;
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        md = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2 (md.x * speed, md.y * speed);
+    }
+
+}
